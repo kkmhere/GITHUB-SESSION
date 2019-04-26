d27d8520173e7fea0482820340d33b153debd745
aman20119 <amansourav5@gmail.com>
2019-04-26 16:57:14 +0530

    hello

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int m,n,temp1,temp2,gcd,lcm,rem;
+    printf("enter the two numbers\n");
+    scanf("%d%d",&m,&n);
+    temp1=m;
+    temp2=n;
+    while(n>0)
+    {
+        rem=m%n;
+        m=n;
+        n=rem;
+    }
+    gcd=m;
+    lcm=(temp1*temp2)/gcd;
+    printf("The GCD of %d and %d elements is %d\n",temp1,temp2,gcd);
+    printf("The LCM of %d and %d elements is %d\n",temp1,temp2,lcm);
     return 0;
 }
